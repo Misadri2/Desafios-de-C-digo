ae07d041d1dbd8578d62d52e3989cc6723ba0d77
Misadri2 <mis.monterio@hotmail.com>
2022-06-05 16:55:10 -0300

    Inclusão desafio CountbyX

---
+++ Codewars/CountbyX.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public class CountbyX
+    {
+        //Criar uma função com dois argumentos que retornarão um array do primeiro número vezes o segundo número
+        public static int[] CountBy(int x, int n)
+        {    
+            var result = Enumerable.Range(1, n).Select(i => i * x).ToArray();           
+            return result;
+           
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -20,6 +20,8 @@ namespace Codewars
 
             //ExDiversos.Billboard("Joana");
 
+            CountbyX.CountBy(2, 5);
+
 
 
 
