ecff3f52555f0a7346195667f95656e4b92faf57
Misadri2 <mis.monterio@hotmail.com>
2022-06-20 21:35:59 -0300

    Inclusão desafio Average e Greet

---
+++ Codewars/Average.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars
+{
+    public class Average
+    {
+
+        public static char GetGrade(int s1, int s2, int s3)
+        {
+            int score = (s1+ s2+ s3) / 3;
+
+            if (score >= 90) return 'A';
+            if (score >= 80) return 'B';
+            if (score >= 70)  return 'C';
+            if (score >= 60 ) return 'D';
+             return 'F';
+        }
+
+
+        public static string Saudação(string name, string owner)
+        {
+            if (name == owner) return "Hello boss";
+            return "Hello guest";
+        }
+
+        //Outra maneira interessante de resolver o mesmo desafio 
+        public static string SaudaçãoGreet(string name, string owner)
+        {
+            return (name == owner) ? "Hello boss" : "Hello guest";
+        }
+    }
+}
