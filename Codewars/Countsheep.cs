68fa4d8a707cee749dbe500f4281b0fa9df30488
Misadri2 <mis.monterio@hotmail.com>
2022-06-26 15:51:51 -0300

    Inclusão desafio CountSheep

---
+++ Codewars/Countsheep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars
+{
+   public class Countsheep
+    {
+        //Em um array de ovelhas/sheeps conta a quantidade de sheep/ovelha;
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int count = 0;                 // contador começando em zero
+            foreach (bool sheep in sheeps) // bool de ovelha em array de ovelhas
+            {
+                if (sheep) count++;        // se for ovelha(true) conta 1
+            }
+            return count;                  // retorna a contagem
+
+        }
+    }
+}
