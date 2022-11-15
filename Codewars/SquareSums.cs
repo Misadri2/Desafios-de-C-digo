6e985baaa9e3105b502cc9cda4333dfc5bad5b23
Misadri2 <mis.monterio@hotmail.com>
2022-11-15 11:57:13 -0300

    add sumTwoSmallestNumbers

--- Codewars/SquareSums.cs
+++ Codewars/SquareSums.cs
@@ -10,6 +10,14 @@ namespace Codewars
         {
             return (int)n.Sum(i => Math.Pow(i, 2));
         }
+
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {         
+            return numbers.OrderBy(i => i).Take(2).Sum();
+        }
+
+        //OrderBy https://learn.microsoft.com/pt-br/dotnet/api/system.linq.enumerable.orderby?view=net-7.0
+        //Take https://learn.microsoft.com/pt-br/dotnet/api/system.linq.enumerable.take?view=net-7.0
     }
 
 }
