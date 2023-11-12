c3085707a94b42aae8496ba8d69308d2a818abf0
Misadri2 <mis.monterio@hotmail.com>
2023-11-12 16:52:10 -0300

    Descrição do desafio do chocolate

--- Codewars/Desafios Codewars/BreakingChocolate.cs
+++ Codewars/Desafios Codewars/BreakingChocolate.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Codewars.Desafios
 {
     public class BreakingChocolate
     {
+
+        //DESCRIPTION:
+        //Your task is to split the chocolate bar of given dimension n x m into small squares.Each square is of size 1x1 and unbreakable.Implement a function that will return minimum number of breaks needed.
+        //For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+        //If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
 
         public static int BreakChocolate(int n, int m)
         {
